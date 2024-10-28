658eb290168298db26cf62776ddb7b548588a567
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-10-28 13:02:42 -0400

    Assignment3 Finished

--- src/App.java
+++ src/App.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("12345", "Jhon", "Doe", 20, "Computer Science");
+        System.out.println(student1);
+
+        Student student2 = new Student("23456", "Jane", "Smith", 21, "Mathmatics");
+        System.out.println(student2);
+
+        Student student3 = new Student("34567", "Bob", "Jhonson", 19, "Physics");
+        System.out.println(student3);
+
+        System.out.println();
+
+    }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Student {
+public record Student(String studentId, String firstName, String lastName, int age, String major) {
 }
